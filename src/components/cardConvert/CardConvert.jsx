4bf774c1dd0dd8b29d691afef8514a3ed21d8a9d
Omar Perez <omar1987perez2@gmail.com>
2023-08-01 21:08:26 +0200

    añadir estilos

--- src/components/cardConvert/CardConvert.jsx
+++ src/components/cardConvert/CardConvert.jsx
@@ -1,30 +1,46 @@
+import { useState } from "react";
 import "./cardconvert.css";
 import exchange from "./Exchange.png";
 import heart from "./Heart.png";
 
 const CardConvert = () => {
+  const [dataSelect, setDataSelect] = useState(["km", "miles"]);
+  const [valueInput, setValueInput] = useState();
+
   return (
     <>
       <section>
         <div className="container-select">
-          <div className="container-select-img">
-            <select name="select-convert">
-              <option value="km">km → miles</option>
-              <option value="miles">miles → km</option>
-              <option value="feet">feet → meter</option>
-              <option value="meter">meter → feet</option>
-              <option value="cm">cm → inch</option>
-              <option value="inch">inch → cm</option>
+          <div>
+            <select
+              name="select-convert"
+              value={dataSelect}
+              onChange={(e) => {
+                const convertArrayAndSeparate = e.target.value.split(",");
+                setDataSelect(convertArrayAndSeparate);
+              }}
+            >
+              <option value={["km", "miles"]}>km → miles</option>
+              <option value={["miles", "km"]}>miles → km</option>
+              <option value={["feet", "meter"]}>feet → meter</option>
+              <option value={["meter", "feet"]}>meter → feet</option>
+              <option value={["cm", "inch"]}>cm → inch</option>
+              <option value={["inch", "cm"]}>inch → cm</option>
             </select>
             <img src={exchange} alt="flechas de conversion" />
           </div>
-          <input type="text" align="right" />
-          <p>km</p>
+          <input
+            type="text"
+            align="right"
+            value={valueInput}
+            onChange={(e) => setValueInput(e.target.value)}
+          />
+          <p>{dataSelect[0]}</p>
         </div>
         <div className="select-convert-container">
           <img src={heart} alt="corazon" />
           <h3>
-            62.12 <span>miles</span>
+            {valueInput} <span>{dataSelect[1]}</span>
           </h3>
         </div>
       </section>
