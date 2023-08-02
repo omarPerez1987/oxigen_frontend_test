b2e3567be1effd94500405be01704b49e386fa70
Omar Perez <omar1987perez2@gmail.com>
2023-08-02 21:06:42 +0200

    añadir funcionalidad

--- src/components/cardConvert/CardConvert.jsx
+++ src/components/cardConvert/CardConvert.jsx
@@ -5,7 +5,33 @@ import heart from "./Heart.png";
 
 const CardConvert = () => {
   const [dataSelect, setDataSelect] = useState(["km", "miles"]);
-  const [valueInput, setValueInput] = useState();
+  const [valueInput, setValueInput] = useState("");
+  
+  const [changeInputByResult, setChangeInputByResult] = useState(false);
+  
+  // conversor de medidas
+  let resConvert = valueInput;
+  switch (dataSelect[0]) {
+    case "km":
+      resConvert = valueInput / 1.609344;
+      break;
+    case "miles":
+      resConvert = valueInput * 1.609344;
+      break;
+    case "feet":
+      resConvert = valueInput * 0.3048;
+      break;
+    case "meter":
+      resConvert = valueInput / 0.3048;
+      break;
+    case "cm":
+      resConvert = valueInput * 0.3937;
+      break;
+    default:
+      resConvert = valueInput * 2.54;
+      break;
+  }
+
 
   return (
     <>
@@ -27,21 +53,48 @@ const CardConvert = () => {
               <option value={["cm", "inch"]}>cm → inch</option>
               <option value={["inch", "cm"]}>inch → cm</option>
             </select>
-            <img src={exchange} alt="flechas de conversion" />
+            <img
+              src={exchange}
+              alt="flechas de conversion"
+              onClick={() =>
+                !changeInputByResult ? setChangeInputByResult(true) : setChangeInputByResult(false) && setValueInput(resConvert)
+              }
+            />
           </div>
-          <input
-            type="text"
-            align="right"
-            value={valueInput}
-            onChange={(e) => setValueInput(e.target.value)}
-          />
-          <p>{dataSelect[0]}</p>
+          {changeInputByResult ? (
+            <>
+              <input
+                type="text"
+                align="right"
+                value={valueInput}
+                onChange={(e) => setValueInput(e.target.value)}
+              />
+              <p>{dataSelect[1]}</p>
+            </>
+
+          ) : (
+            <>
+              <input
+                type="text"
+                align="right"
+                value={valueInput}
+                onChange={(e) => setValueInput(e.target.value)}
+              />
+              <p>{dataSelect[0]}</p>
+            </>
+          )}
         </div>
         <div className="select-convert-container">
           <img src={heart} alt="corazon" />
-          <h3>
-            {valueInput} <span>{dataSelect[1]}</span>
-          </h3>
+          {changeInputByResult ? (
+            <h3>
+              {valueInput} <span>{dataSelect[0]}</span>
+            </h3>
+          ) : (
+            <h3>
+              {resConvert.toFixed(2)} <span>{dataSelect[1]}</span>
+            </h3>
+          )}
         </div>
       </section>
     </>
