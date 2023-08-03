ed31505ece25c1af15bb990b053deb4d52873c03
Alpha870 <omar1987perez@gmail.com>
2023-08-03 17:56:09 +0200

    añado otra funcionalidad

--- src/components/cardConvert/CardConvert.jsx
+++ src/components/cardConvert/CardConvert.jsx
@@ -3,15 +3,15 @@ import "./cardconvert.css";
 const CardConvert = () => {
   return (
     <section className="container-convert">
-      <h2>convert</h2>
+      <h2 className="title-convert">convert</h2>
       <div>
         <select name="select-convert" className="select-convert">
           <option value="km-mi">km → miles</option>
           <option value="mi-km">miles → km</option>
-          <option value="ft-m">feet → metros</option>
-          <option value="m-ft">metros → feet</option>
-          <option value="cm-in">cm → pulgadas</option>
-          <option value="in-cm">pulgadas → cm</option>
+          <option value="ft-m">feet → meter</option>
+          <option value="m-ft">meter → feet</option>
+          <option value="cm-in">cm → inch</option>
+          <option value="in-cm">inch → cm</option>
         </select>
       </div>
     </section>
